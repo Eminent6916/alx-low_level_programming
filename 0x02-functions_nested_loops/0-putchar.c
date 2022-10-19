7f24899d07653cf5b0c060623055fb563491734d
Ologunde Elisha <ologelisha@gmail.com>
2022-10-18 18:58:11 -0700

    alx-functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,26 @@
 #include "main.h"
 
 /**
- * main - print with _putchar
- *written by O.Elisha
- *Return: 0
+ * main - Entry Point
+ *
+ * Description: A program that prints _putchar,
+ * followed by a new line.
+ *
+ * Return: 0(Success)
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char word[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(word[i]);
+	}
+
+	_putchar('\n');
+
+	return (0);
 }
